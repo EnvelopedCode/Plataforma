c3ff6c1e385392475942d5aaf387f3d7ac495820
EnvelopedCode <nilzong@uninorte.edu.co>
2021-11-25 23:18:22 -0500

    Reestructuracion del proyecto (Version 1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import logo from './("You can use a .svg here")';
+// Here we import all final components and merge them into the parent component, so then it gets sent to index.js
 
 function App() {
   return (
